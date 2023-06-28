7cfd57dbf9e1ad93640928f3b9b7d532cd02cbac
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-06-28 14:18:23 +0300

    👔New serializers

---
+++ core/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserCreateRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+
+class UserCreateResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name"]
+
+
+class LoginRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+class LoginResponseSerializer(serializers.Serializer):
+    token = serializers.CharField()
