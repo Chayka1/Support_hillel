a1f10c1a4b380df264aeb62b8a47c535357095a4
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-08-02 16:58:55 +0300

    🐛Fix bug

--- users/api.py
+++ users/api.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from users.serializers import UserCreateSerializer  # noqa
@@ -8,6 +9,8 @@ from users.serializers import UserRegistrationResponseSerializer
 
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
+
+    permission_classes = [AllowAny]
 
     def post(self, request):
         create_serializer = self.get_serializer(data=request.data)
