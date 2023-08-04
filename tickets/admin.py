234bc69fc2615ac137a68aa8c15e34aabc821979
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-08-04 20:55:21 +0300

    🐛Fix

--- tickets/admin.py
+++ tickets/admin.py
@@ -5,9 +5,7 @@ from tickets.models import Ticket
 
 @admin.register(Ticket)
 class UserAdmin(admin.ModelAdmin):
-    readonly_fields = [
-        "user",
-    ]
+    readonly_fields = ["user", "title", "text"]
     list_display = ["title", "user", "manager", "status"]
     list_filter = ["status", "manager"]
     search_fields = ["title"]
