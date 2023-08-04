a3f48e80f84afd3f25f90b60b4a58f8439f8d693
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-08-04 19:20:16 +0300

    🎉Add admin panel for tickets

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,1 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from tickets.models import Ticket
+
+
+@admin.register(Ticket)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "user",
+    ]
+    list_display = ["title", "user", "manager", "status"]
+    list_filter = ["status", "manager"]
+    search_fields = ["title"]
